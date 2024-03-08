3a716da295518ae19d58828eb4987b96e86abb03
bahaar-ghafari <zahraghafari1996@gmail.com>
2024-03-08 12:48:24 +0100

    feat: remove ionic

--- src/pages/app/App.tsx
+++ src/pages/app/App.tsx
@@ -1,9 +1,6 @@
 import React from "react";
-import { setupIonicReact } from "@ionic/react";
 import { BrowserRouter } from "react-router-dom";
 import PT_Routes from "../../components/Routes/PT_Routes";
-
-setupIonicReact();
 
 const App: React.FC = () => (
     <BrowserRouter>
